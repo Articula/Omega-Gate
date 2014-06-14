11f07c3dded6abc73c8b29f1ac281badf64ecd72
Faye Doherty <articula@hotmail.com>
2014-06-14 16:36:29 -0700

    Added basic connection to MySQL database. Using MySQL Connector/Net v6.6.6 (dev.mysql.com/downloads/connector/net), MySql.Data now referenced and .dll generated on build.

--- Server C# Application/MainForm.cs
+++ Server C# Application/MainForm.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using System.Xml;
 using System.Timers;
 using System.IO;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace SpaceStrategySystem
 {
@@ -51,6 +53,7 @@ namespace SpaceStrategySystem
         private UpdateFrequency m_frequency = UpdateFrequency.Hourly;
         private DateTime m_nextUpdate;
         private System.Timers.Timer m_timer;
+        private MySqlConnection m_connection;
         private Dictionary<UpdateFrequency, string> m_frequencyStrings = new Dictionary<UpdateFrequency,string>();
         private Dictionary<string, UpdateFrequency> m_frequencyEnums = new Dictionary<string,UpdateFrequency>();
 
@@ -61,6 +64,8 @@ namespace SpaceStrategySystem
             InitializeComponent();
             this.PopulateFrequencyStrings();
 
+            this.DBConnectionTest();
+
             //Extract XML values and update attributes
             this.ReadXML();
             this.SetFrequencyCombo();
@@ -70,6 +75,86 @@ namespace SpaceStrategySystem
             this.m_timer = new System.Timers.Timer(ONE_MINUTE);
             this.m_timer.Elapsed += new ElapsedEventHandler(this.RunCheck);
             this.m_timer.Start();
+        }
+
+        private void DBConnectionTest()
+        {
+            string server = "localhost";
+            string database = "connectiontest";
+            string uid = "root";
+            string password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            //MySqlConnection connection = new MySqlConnection(connectionString);
+            this.m_connection = new MySqlConnection(connectionString);
+            //connection.Close();
+
+            if(this.OpenConnection())
+            {
+                //Select and grab current value
+                string selectQuery = "SELECT * FROM testtable";
+                List<int> list = new List<int>();
+                MySqlCommand selectcmd = new MySqlCommand(selectQuery, this.m_connection);
+                MySqlDataReader dataReader = selectcmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    list.Add(Convert.ToInt32(dataReader["testint"]));
+                }
+                dataReader.Close();
+
+                //Update value
+                string updateQuery = "UPDATE testtable SET testint=" + (list[0] + 1).ToString();
+                MySqlCommand updatecmd = new MySqlCommand();
+                updatecmd.CommandText = updateQuery;
+                updatecmd.Connection = this.m_connection;
+                updatecmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                m_connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                m_connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
         }
 
         private void PopulateFrequencyStrings()
@@ -133,6 +218,7 @@ namespace SpaceStrategySystem
 
         private void UpdateFrequencyXML()
         {
+            /*TODO: Put in another check for XML file (/generate if not) here*/
             XmlDocument file = new XmlDocument();
             file.Load(CONFIG_FILE);
             XmlNode node = file.SelectSingleNode(XMLFields.Configuration + "/" + XMLFields.Frequency);
@@ -189,6 +275,7 @@ namespace SpaceStrategySystem
              * Action long list of database updates
              * Close connection to database*/         
 
+            /*TODO: Put in another check for XML file (/generate if not) here*/
             //Update last update in XML
             XmlDocument file = new XmlDocument();
             file.Load(CONFIG_FILE);
